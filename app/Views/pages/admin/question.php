79def70f1a87a43f6cf3acbb1e4745bb887e2936
Michael Kemisola <121091920+khemzy71@users.noreply.github.com>
2025-08-22 14:29:01 +0100

    Add files via upload

--- app/Views/pages/admin/question.php
+++ app/Views/pages/admin/question.php
@@ -1,4 +1,4 @@
 <?= $this->extend('layouts/adminLayouts') ?>
     <?= $this->section("pagecontent") ?>
-        <?= $this->include('partials/admin/dashboard') ?>
+        <?= $this->include('partials/admin/question') ?>
     <?= $this->endSection() ?>
